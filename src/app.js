3f991f5ec34d0dd4ec590335673a60ba1af6dddc
ARaquel24 <anarklprzrjs@gmail.com>
2024-07-30 02:52:10 +0000

    Finish excuse

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector("#excuse").innerHTML = excusas();
   console.log("Hello Rigo from the console!");
 };
+
+function excusas() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let quien = Math.floor(Math.random() * who.length);
+  let accion = Math.floor(Math.random() * action.length);
+  let que = Math.floor(Math.random() * what.length);
+  let donde = Math.floor(Math.random() * when.length);
+  return (
+    who[quien] + " " + action[accion] + " " + what[que] + " " + when[donde]
+  );
+}
